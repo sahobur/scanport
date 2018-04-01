76257e0d2e91391c8b77e855e17a53b3ac08e60f
SahoHome <278999@mail.ru>
2018-04-01 21:12:57 +0800

    v0.1 done

--- getports.go
+++ getports.go
@@ -120,7 +120,8 @@ var ifOperStatus string = "1.3.6.1.2.1.2.2.1.8"
 var ifSpeed string = "1.3.6.1.2.1.31.1.1.1.15"
 var ifDuplex string = "1.3.6.1.2.1.10.7.2.1.19"
 var ifName string = "1.3.6.1.2.1.31.1.1.1.1"
-var ifStatusDlink3028 string  = "1.3.6.1.4.1.171.11.63.6.2.2.1.1.5"
+var ifStatusDlink3028 string = "1.3.6.1.4.1.171.11.63.6.2.2.1.1.5"
+var ifStatusDlink3526 string = "1.3.6.1.4.1.171.11.64.1.2.4.4.1.6"
 
 type Hosts struct {
 	id        int16
@@ -134,6 +135,7 @@ type Interfaces struct {
 	InterfacesSpeed  uint64
 	InterfacesStatus uint64
 }
+
 func processCisco(ip string, community string) {
 	//fmt.Println("IP: ", h.ip, "  Device model: Cisco")
 	ifs := make([]*Interfaces, 0)
@@ -352,8 +354,8 @@ func processHuaweiS23(ip string, community string) {
 	}
 	//panic("STOP")
 }
-func processDES3028(ip string , community string) {
-	fmt.Println("IP: ",ip,"  DLINK 3028  ")
+func processSpecDlink(ip string, community string, model string) {
+	//fmt.Println("IP: ",ip,"  DLINK 3028  ")
 	g.Default.Community = community
 	g.Default.Target = ip
 	g.Default.Timeout = 5000000000
@@ -363,39 +365,77 @@ func processDES3028(ip string , community string) {
 		fmt.Print("host:=", ip, " ")
 		log.Println("Connect() err: ", err)
 	}
-	resultOperStatus, err2 := g.Default.BulkWalkAll(ifStatusDlink3028)
+	oidd := ""
+	if model == "3028" {
+		oidd = ifStatusDlink3028
+	}
+	if model == "3526" {
+		oidd = ifStatusDlink3526
+	}
+	resultOperStatus, err2 := g.Default.BulkWalkAll(oidd)
 	if err2 != nil {
 		fmt.Printf("Walk Error: %v\n", err)
 		os.Exit(1)
 	}
+	if model == "3028" {
+		for _, r := range resultOperStatus {
+			aoid := strings.Split(r.Name, ".")
+			ifindex, err := strconv.Atoi(aoid[16])
+			if err != nil {
+				panic("error string conv")
+			}
+			portstate := g.ToBigInt(r.Value).Uint64()
 
-	for _, r := range resultOperStatus {
-		aoid := strings.Split(r.Name, ".")
-		ifindex, err := strconv.Atoi(aoid[16])
-		if err != nil {
-			panic("error string conv")
-		}
-		portstate := g.ToBigInt(r.Value).Uint64()
-
-		if portstate == 2 || portstate == 3 || portstate == 4 {
-			switch portstate {
+			if portstate == 2 || portstate == 3 || portstate == 4 {
+				switch portstate {
 				//2 10h
 				//3 10f
 				//4 100h
 				// 5 100f
-			case 2:
-				fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 10")
-			case 3:
-				fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  FULL / 10")
-			case 4:
-				fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 100")
+				case 2:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 10")
+				case 3:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  FULL / 10")
+				case 4:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 100")
+				}
+
 			}
+			//fmt.Println("Port: ",ifindex,"  State: ",r.Value)
+
+			//fmt.Println("Name OID: ", r.Name, "  Duplex: ", duplex)
 
 		}
-		//fmt.Println("Port: ",ifindex,"  State: ",r.Value)
-		
-		//fmt.Println("Name OID: ", r.Name, "  Duplex: ", duplex)
+	}
+	if model == "3526" {
+		for _, r := range resultOperStatus {
+			aoid := strings.Split(r.Name, ".")
+			ifindex, err := strconv.Atoi(aoid[16])
+			if err != nil {
+				panic("error string conv")
+			}
+			portstate := g.ToBigInt(r.Value).Uint64()
 
+			if portstate == 3 || portstate == 4 || portstate == 5 {
+				switch portstate {
+				//2 10h
+				//3 10f
+				//4 100h
+				// 5 100f
+				case 3:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 10")
+				case 4:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  FULL / 10")
+				case 5:
+					fmt.Println("IP: ", ip, " IF: ", ifindex, "  DUPLEX/SPEED  HALF / 100")
+				}
+
+			}
+			//fmt.Println("Port: ",ifindex,"  State: ",r.Value)
+
+			//fmt.Println("Name OID: ", r.Name, "  Duplex: ", duplex)
+
+		}
 	}
 
 }
@@ -403,8 +443,9 @@ func processStandart(ip string, community string) {
 	ifs := make([]*Interfaces, 0)
 	g.Default.Community = community
 	g.Default.Target = ip
-	g.Default.Timeout = 5000000000
+	g.Default.Timeout = 90000900000
 	g.Default.Retries = 5
+	g.Default.MaxRepetitions = 44
 	err := g.Default.Connect()
 	//var ifindex []int16  = 0
 	if err != nil {
@@ -447,7 +488,7 @@ func processStandart(ip string, community string) {
 		//fmt.Println("Name OID: ", r.Name, "  Duplex: ", duplex)
 
 	}
-	endIfindex := startIfindex + len(ifs) -1
+	endIfindex := startIfindex + len(ifs) - 1
 	//fmt.Println("Start index: ", startIfindex, "  End ifindex: ", endIfindex)
 	// get oper status of port
 	i = 0
@@ -538,105 +579,135 @@ func main() {
 		if h.community != "" {
 			if desc.Contains(h.Descr, "Cisco") {
 				//processCisco(h.ip, h.community)
-				processStandart(h.ip,h.community)
+				processStandart(h.ip, h.community)
 			}
 
 			if desc.Contains(h.Descr, "S2328") {
 
 				//processHuaweiS23(h.ip, h.community)
-				processStandart(h.ip,h.community)
+				processStandart(h.ip, h.community)
 			}
 
 			if desc.Contains(h.Descr, "DES-3028") {
-				processDES3028(h.ip,h.community)
+				processSpecDlink(h.ip, h.community, "3028")
 			}
 
-			/*
-				if desc.Contains(h.Descr, "DES-3200-10") {
-					fmt.Println("IP: ", h.ip, "  Device model: DES-3200-10")
-				}
-				if desc.Contains(h.Descr, "DES-3200-28") {
-					fmt.Println("IP: ", h.ip, "  Device model: DES-3200-10")
-				}
-				if desc.Contains(h.Descr, "DES-1210-28") {
-					fmt.Println("IP: ", h.ip, "  Device model: DES-1210-28")
-				}
-				if desc.Contains(h.Descr, "DES-2108") {
-					fmt.Println("IP: ", h.ip, "  Device model: DES-2108")
-				}
-				if desc.Contains(h.Descr, "DES-3526") {
-					fmt.Println("IP: ", h.ip, "  Device model: DES-3526")
-				}
+			if desc.Contains(h.Descr, "DES-3200-10") {
+				//fmt.Println("IP: ", h.ip, "  Device model: DES-3200-10")
+				processStandart(h.ip, h.community)
+			}
+			if desc.HasPrefix(h.Descr, "DES-3200-28") {
+				//fmt.Println("IP:", h.ip, "DES-3200-28")
+				processStandart(h.ip, h.community)
+			}
 
+			if desc.HasPrefix(h.Descr, "D-Link DES-3200-28") {
+				//fmt.Println("IP:", h.ip, "D-Link DES-3200-28")
+				processStandart(h.ip, h.community)
+			}
+
+			if desc.Contains(h.Descr, "DES-1210-28") {
+				processStandart(h.ip, h.community)
+
+			}
+
+			if desc.Contains(h.Descr, "DES-2108") {
+				fmt.Println("IP: ", h.ip, " UNABLE GET SNMP PORT STATE FOR DES-2108")
+				//processDES3028(h.ip, h.community)
+			}
+
+			if desc.Contains(h.Descr, "DES-3526") {
+				//fmt.Println("IP: ", h.ip, "  Device model: DES-3526")
+				processSpecDlink(h.ip, h.community, "3526")
+			}
+			
 				if desc.Contains(h.Descr, "DGS-3120-24SC") {
-					fmt.Println("IP: ", h.ip, "  Device model: DGS-3120-24SC")
-
+				//	fmt.Println("IP: ", h.ip, "  Device model: DGS-3120-24SC")
+					processStandart(h.ip,h.community)
 				}
 				if desc.Contains(h.Descr, "DGS-3700-12G") {
-					fmt.Println("IP: ", h.ip, "  Device model: DGS-3700-12G")
-
+				//	fmt.Println("IP: ", h.ip, "  Device model: DGS-3700-12G")
+					processStandart(h.ip,h.community)
 				}
+				
 				if desc.Contains(h.Descr, "ES-2024A") {
-					fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-2024A")
+				//	fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-2024A")
+					processStandart(h.ip,h.community)
 
 				}
+				
 				if desc.Contains(h.Descr, "ES-3124") {
-					fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-3124")
-
+					//fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-3124")
+					processStandart(h.ip,h.community)
 				}
+				
 				if desc.Contains(h.Descr, "ES-3148") {
-					fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-3148")
+				//	fmt.Println("IP: ", h.ip, "  Device model: Zyxel ES-3148")
+					processStandart(h.ip,h.community)
 
 				}
+				
 				if desc.Contains(h.Descr, "ISCOM2110") {
-					fmt.Println("IP: ", h.ip, "  Device model: ISCOM2110")
+				//	fmt.Println("IP: ", h.ip, "  Device model: ISCOM2110")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "ISCOM2128") {
-					fmt.Println("IP: ", h.ip, "  Device model: ISCOM2128")
+				//	fmt.Println("IP: ", h.ip, "  Device model: ISCOM2128")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "MES-1024") {
-					fmt.Println("IP: ", h.ip, "  Device model: MES-1024 v < 1.1.30")
+				//	fmt.Println("IP: ", h.ip, "  Device model: MES-1024 v < 1.1.30")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "MES-1124") || desc.Contains(h.Descr, "MES1124") {
-					fmt.Println("IP: ", h.ip, "  Device model: MES-1124")
+				//	fmt.Println("IP: ", h.ip, "  Device model: MES-1124")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "MES-2124") || desc.Contains(h.Descr, "MES2124") {
-					fmt.Println("IP: ", h.ip, "  Device model: MES-2124")
+				//	fmt.Println("IP: ", h.ip, "  Device model: MES-2124")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "MES1024") {
-					fmt.Println("IP: ", h.ip, "  Device model: MES-1024 version > 1.1.30")
+				//	fmt.Println("IP: ", h.ip, "  Device model: MES-1024 version > 1.1.30")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "MES3124") {
-					fmt.Println("IP: ", h.ip, "  Device model: MES 3124")
+				//	fmt.Println("IP: ", h.ip, "  Device model: MES 3124")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "ROS") {
-					fmt.Println("IP: ", h.ip, "  Device model: Risecom ROS 28 port")
+				//	fmt.Println("IP: ", h.ip, "  Device model: Risecom ROS 28 port")
+					processStandart(h.ip,h.community)
 
 				}
 
 				if desc.Contains(h.Descr, "SNR-S2940") {
-					fmt.Println("IP: ", h.ip, "  Device model: SNR-S2940")
+				//	fmt.Println("IP: ", h.ip, "  Device model: SNR-S2940")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "SNR-S2950-24G") {
-					fmt.Println("IP: ", h.ip, "  Device model: SNR-S2950-24G")
+				//	fmt.Println("IP: ", h.ip, "  Device model: SNR-S2950-24G")
+					processStandart(h.ip,h.community)
 
 				}
 				if desc.Contains(h.Descr, "SNR-S2960-24G") {
-					fmt.Println("IP: ", h.ip, "  Device model: SNR-S2960-24G")
+				//	fmt.Println("IP: ", h.ip, "  Device model: SNR-S2960-24G")
+					processStandart(h.ip,h.community)
 
 				}
 				if h.Descr == "" {
 					fmt.Println("IP: ", h.ip, "  UNKNOWN DEVICE")
 				}
-			*/
+			
 		}
 	}
+	fmt.Println("Done.")
 }
